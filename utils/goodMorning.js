56b4e61e665c830d90ed1d29141fc50309e34375
Your Name <you@example.com>
2025-05-12 22:33:39 +0700

    netflix and chill

--- utils/goodMorning.js
+++ utils/goodMorning.js
@@ -29,6 +29,20 @@ function scheduleGoodMorning(client) {
         }
     });
 
+    // Netflix and chill
+    schedule.scheduleJob('00 22 * * 4', async () => {
+        const now = moment().tz('Asia/Ho_Chi_Minh');
+        if (now.hour() === 21 && now.minute() === 59) {
+            const channel = await client.channels.fetch('1258057651800113224');
+            if (channel && channel.isTextBased()) {
+                channel.send({
+                    content: `@everyone Đến lúc xem phim rồi`,
+                    allowedMentions: { parse: ['everyone'] }
+                });
+            }
+        }
+    });
+
 }
 
 module.exports = { scheduleGoodMorning };
